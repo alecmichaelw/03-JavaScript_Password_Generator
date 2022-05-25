127a03cfa0952420f9b25734905277028cddfdef
Alec Westbrook <alecmichaelw@gmail.com>
2022-05-25 04:00:16 -0400

    following tutorial

--- script.js
+++ script.js
@@ -12,6 +12,17 @@ const includeSymbolsElement = document.getElementById
 const form = document.getElementById
 ('passwordGeneratorForm')
 
+const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90)
+const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
+const NUMBERS_CHAR_CODES = arrayFromLowToHigh(48, 57)
+const SYMBOLS_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(
+  arrayFromLowToHigh(58, 64)
+).concat(
+  arrayFromLowToHigh(91, 96)
+).concat(
+  arrayFromLowToHigh(123, 126)
+)
+
 characterAmountNumber.addEventListener('input', syncCharacterAmount)
 characterAmountRange.addEventListener('input', syncCharacterAmount)
 
@@ -21,14 +32,27 @@ form.addEventListener('submit', e => {
   const includeNumbers = includeNumbersElement.Checked
   const includeSymbols = includeSymbolsElement.Checked
   const password = generatePassword(characterAmount, includeUppercase,
-  includeNumbers, IncludeSymbols)
+  includeNumbers, includeSymbols)
 })
 
 function generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols) {
-  String.fromCharCode(65)
+  console.log(LOWERCASE_CHAR_CODES)
+  let charCodes = LOWERCASE_CHAR_CODES
+  if (includeUppercase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES)
+  if (includeNumbers) charCodes = charCodes.concat(NUMBERS_CHAR_CODES)
+  if (includeSymbols) charCodes = charCodes.concat(SYMBOLS_CHAR_CODES)
+
+  const passwordCharacters = []
+  for (let i = 0; i < characterAmount, i ++) {
+    const characterCode = charCodes[Math.floor(math.random() *
+      characterAmount)]
+    passwordCharacters.push(string.fromCharCode(characterCode))
+  }
+  return passwordCharacters.join('')
 }
 
-function fromLowToHigh(low, high) {
+function arrayFromLowToHigh(low, high) {
+  const array = []
   for (let i = low; i <= high; i++) {
     array.push(i)
   }
@@ -43,16 +67,16 @@ function syncCharacterAmount(e) {
 }
 
 
-// Assignment Code
+// // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+//   passwordText.value = password;
 
-}
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// }
+// // Add event listener to generate button
+generateBtn.addEventListener("click", generatePassword);
