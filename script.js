9721ed4d2d02c4fecd27e078a424047f1c20c7c3
Alec Westbrook <alecmichaelw@gmail.com>
2022-05-30 09:11:13 -0400

    fixed broken funtion; deleted textarea from html

--- script.js
+++ script.js
@@ -1,17 +1,10 @@
-const characterAmountRange = document.getElementById
-('characterAmountRange')
-const characterAmountNumber = document.getElementById
-('characterAmountNumber')
-const includeUppercaseElement = document.getElementById
-('includeUppercase')
-const includeNumbersElement = document.getElementById
-('includeNumbers')
-const includeSymbolsElement = document.getElementById
-('includeSymbols')
-const form = document.getElementById
-('passwordGeneratorForm')
-const password = document.getElementById
-('password')
+const characterAmountRange = document.getElementById('characterAmountRange')
+const characterAmountNumber = document.getElementById('characterAmountNumber')
+const includeUppercaseElement = document.getElementById('includeUppercase')
+const includeNumbersElement = document.getElementById('includeNumbers')
+const includeSymbolsElement = document.getElementById('includeSymbols')
+const form = document.getElementById('passwordGeneratorForm')
+const passwordDisplay = document.getElementById('passwordDisplay')
 
 const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90)
 const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
@@ -28,17 +21,18 @@ characterAmountNumber.addEventListener('input', syncCharacterAmount)
 characterAmountRange.addEventListener('input', syncCharacterAmount)
 
 form.addEventListener('submit', e => {
+  e.preventDefault()
   const characterAmount = characterAmountNumber.value
-  const includeUppercase = includeUppercaseElement.Checked
-  const includeNumbers = includeNumbersElement.Checked
-  const includeSymbols = includeSymbolsElement.Checked
+  const includeUppercase = includeUppercaseElement.checked
+  const includeNumbers = includeNumbersElement.checked
+  const includeSymbols = includeSymbolsElement.checked
   const password = generatePassword(characterAmount, includeUppercase,
   includeNumbers, includeSymbols)
-  password.innerText = password
+  passwordDisplay.innerText = password
 })
 
 function generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols) {
-  console.log(LOWERCASE_CHAR_CODES)
+  console.log(UPPERCASE_CHAR_CODES)
   let charCodes = LOWERCASE_CHAR_CODES
   if (includeUppercase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES)
   if (includeNumbers) charCodes = charCodes.concat(NUMBERS_CHAR_CODES)
@@ -46,9 +40,8 @@ function generatePassword(characterAmount, includeUppercase, includeNumbers, inc
 
   const passwordCharacters = []
   for (let i = 0; i < characterAmount; i++) {
-    const characterCode = charCodes[Math.floor(math.random() *
-      characterAmount)]
-    passwordCharacters.push(string.fromCharCode(characterCode))
+    const characterCode = charCodes[Math.floor(Math.random() * charCodes.length)]
+    passwordCharacters.push(String.fromCharCode(characterCode))
   }
   return passwordCharacters.join('')
 }
